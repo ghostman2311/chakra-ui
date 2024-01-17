a5a0458905cb4d378a31830b3a108267f058efe4
Nikhil <nkarnwal07@gmail.com>
2024-01-17 20:59:10 +0530

    notifications page added

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Layout from "./components/Layout";
 import AccountSetting from "./pages/AccountSetting";
 import Dashboard from "./pages/Dashboard";
 import Downloads from "./pages/Downloads";
+import Notifications from "./pages/NotificationSetting";
 import Subscription from "./pages/Subscription";
 import { Route, Routes } from "react-router-dom";
 
@@ -13,6 +14,7 @@ function App() {
         <Route path="downloads" element={<Downloads />} />
         <Route path="subscriptions" element={<Subscription />} />
         <Route path="settings" element={<AccountSetting />} />
+        <Route path="notifications" element={<Notifications />} />
       </Route>
     </Routes>
   );
